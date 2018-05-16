018cf06871b338a94ae8fa6ac6456777ecd161a4
Rahul Arulkumaran <rahuldravid313@gmail.com>
2018-05-16 23:08:04 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 from setuptools import setup
+import os
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-
-    name='python-syntaxdb',   
-    version='0.1.1',                          
-    scripts=['syntaxdb']
-
+    name = "python-syntaxdb",
+    version = "0.0.1",
+    author = "Rahul Arulkumaran",
+    author_email = "rahulkumaran@gmail.com",
+    description = ("A Python moudle for accessing the SyntaxDB API"),
+    license = "MIT",
+    keywords = "syntaxdb",
+    url = "https://github.com/rahulkumaran/python-syntaxdb",
+    packages=['syntaxdb'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
+    install_requires=[
+        "requests",
+    ],
+    package_data={
+        "": ["README.md", "LICENSE.md"]
+    }
 )
